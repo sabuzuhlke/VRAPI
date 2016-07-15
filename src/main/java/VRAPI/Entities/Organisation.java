a83902f46f98f8b4221aa8b0913805e56c87a902
Samuel Budd <sabu@Samuels-MacBook-Pro.local>
2016-07-15 10:39:34 +0100

    common representation of organisation

---
+++ src/main/java/VRAPI/Entities/Organisation.java
@@ -0,0 +1,170 @@
+package VRAPI.Entities;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Organisation {
+
+    private Long vertecId;
+    private String ownedOnVertecBy;
+    private Boolean active;
+
+    private String supervisingEmail;
+
+    private String name;
+    private String website;
+    private String category;
+    private String businessDomain;
+    private String buildingName;
+    private String street_no;
+    private String street;
+    private String city;
+    private String country;
+    private String zip;
+
+    private String modified;
+    private String created;
+
+    public Organisation() {
+    }
+
+    public String toJSONString(){
+        String retStr = null;
+        ObjectMapper m = new ObjectMapper();
+        try{
+
+            retStr = m.writerWithDefaultPrettyPrinter().writeValueAsString(this);
+        }
+        catch(Exception e){
+            System.out.println("Could not convert XML Envelope to JSON: " + e.toString());
+        }
+        return retStr;
+    }
+
+    public Long getVertecId() {
+        return vertecId;
+    }
+
+    public void setVertecId(Long vertecId) {
+        this.vertecId = vertecId;
+    }
+
+    public String getOwnedOnVertecBy() {
+        return ownedOnVertecBy;
+    }
+
+    public void setOwnedOnVertecBy(String ownedOnVertecBy) {
+        this.ownedOnVertecBy = ownedOnVertecBy;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public String getSupervisingEmail() {
+        return supervisingEmail;
+    }
+
+    public void setSupervisingEmail(String supervisingEmail) {
+        this.supervisingEmail = supervisingEmail;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getBusinessDomain() {
+        return businessDomain;
+    }
+
+    public void setBusinessDomain(String businessDomain) {
+        this.businessDomain = businessDomain;
+    }
+
+    public String getBuildingName() {
+        return buildingName;
+    }
+
+    public void setBuildingName(String buildingName) {
+        this.buildingName = buildingName;
+    }
+
+    public String getStreet_no() {
+        return street_no;
+    }
+
+    public void setStreet_no(String street_no) {
+        this.street_no = street_no;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getModified() {
+        return modified;
+    }
+
+    public void setModified(String modified) {
+        this.modified = modified;
+    }
+
+    public String getCreated() {
+        return created;
+    }
+
+    public void setCreated(String created) {
+        this.created = created;
+    }
+}
