397ebba2932dffbf3ff81310539f4545bceea8c9
Samuel Budd <sabu@samuels-mbp.ads.zuehlke.com>
2016-07-20 15:00:22 +0100

    updated vertec server ip address

--- src/main/java/VRAPI/VertecServerInfo.java
+++ src/main/java/VRAPI/VertecServerInfo.java
@@ -1,11 +1,16 @@
 package VRAPI;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class VertecServerInfo {
-    public static final String VERTEC_SERVER_HOST = "172.18.112.101";
+    public static final String VERTEC_SERVER_HOST = "172.18.112.87";
     public static final String VERTEC_SERVER_PORT = "8095";
 
     public static final Integer BAD_REQUEST  = 3;
     public static final Integer UNAUTHORISED = 2;
     public static final Integer FORBIDDEN    = 1;
     public static final Integer AUTHORISED   = 0;
+
+    public static final Logger log = LoggerFactory.getLogger("TestLogger");
 }
